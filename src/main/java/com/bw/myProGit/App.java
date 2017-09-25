9668f6fe7c6fe8d95dd7990d076935d2c2eff52e
lkhws <1598087143@qq.com>
2017-09-25 23:01:19 +0800

    第一次提交项目

---
+++ src/main/java/com/bw/myProGit/App.java
@@ -0,0 +1,13 @@
+package com.bw.myProGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
